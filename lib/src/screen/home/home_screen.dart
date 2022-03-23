5635d7d09dcef8b894a9415feb77459a0ed47ad7
Phúc Hồ <hophuc236@gmail.com>
2022-03-23 16:24:49 +0700

    update calendar

--- lib/src/screen/home/home_screen.dart
+++ lib/src/screen/home/home_screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:mission_timer/core/helper/utils/theme_data.dart';
-import 'package:mission_timer/src/widget/basics_calendar.dart';
+import 'package:mission_timer/src/widget/calendar/calendar.dart';
 import 'package:mission_timer/src/widget/menu_bar.dart';
 
 class HomeScreen extends StatelessWidget with ThemeDataMixin {
   static const String router = "/HomeScreen";
   @override
   Widget build(BuildContext context) {
-    return TableComplexExample();
+    return Scaffold(
+      drawer: MenuBar(),
+      appBar: AppBar(),
+      body: Calendar(),
+    );
   }
 }
