167ede4514362b1fa9ee39fcf0d6dfb61c029f66
Phúc Hồ <hophuc236@gmail.com>
2022-04-25 23:11:46 +0700

    update app notification in ios

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Gonosen on 25/04/2022.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
